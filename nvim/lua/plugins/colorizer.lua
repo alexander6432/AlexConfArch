7b4f474b34b8d92dcc3048059992cbbf9c943715
Alexander Gallardo <alexander6432@gmail.com>
2025-01-03 00:09:12 -0600

    config nvim colorizer

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,20 @@
+return {
+  "norcalli/nvim-colorizer.lua",
+  event = "VeryLazy",
+  config = function()
+    require("colorizer").setup({
+      "*",
+      css = {
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        RRGGBBAA = true,
+        rgb_fn = false,
+        hsl_fn = false,
+        css = false,
+        css_fn = false,
+        mode = "foreground",
+      },
+    }, { mode = "background" })
+  end,
+}
