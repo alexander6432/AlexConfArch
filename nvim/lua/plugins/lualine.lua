b71266f542bec20fa6c663f6824acf43ea733bf5
Alexander Gallardo <alexander6432@gmail.com>
2025-01-18 17:24:25 -0600

    nvim se cambio de cmp a blink y añadio navic

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
+  optional = true,
   event = "VeryLazy",
   init = function()
     vim.g.lualine_laststatus = vim.o.laststatus
@@ -11,7 +12,7 @@ return {
       vim.o.laststatus = 0
     end
   end,
-  opts = function()
+  opts = function(_, opts)
     -- PERF: we don't need this lualine require madness 🤷
     local lualine_require = require("lualine_require")
     lualine_require.require = require
@@ -106,22 +107,8 @@ return {
 
     -- do not add trouble symbols if aerial is enabled
     -- And allow it to be overriden for some buffer types (see autocmds)
-    if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-      local trouble = require("trouble")
-      local symbols = trouble.statusline({
-        mode = "symbols",
-        groups = {},
-        title = false,
-        filter = { range = true },
-        format = "{kind_icon}{symbol.name:Normal}",
-        hl_group = "lualine_c_normal",
-      })
-      table.insert(opts.sections.lualine_c, {
-        symbols and symbols.get,
-        cond = function()
-          return vim.b.trouble_lualine ~= false and symbols.has()
-        end,
-      })
+    if vim.g.trouble_lualine then
+      table.insert(opts.sections.lualine_c, { "navic", color_correction = "dynamic" })
     end
 
     return opts
